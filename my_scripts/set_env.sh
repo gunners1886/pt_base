cb696d170857b7b2da5a47764c3d0b63c48b1abb
andy yang <huaweipc@example.com>
2024-04-06 23:39:22 +0800

    1

---
+++ my_scripts/set_env.sh
@@ -0,0 +1,6 @@
+conda activate pt_base
+
+
+# pip3 install torch torchvision torchaudio  -i http://pypi.tuna.tsinghua.edu.cn/simple --trusted-host pypi.tuna.tsinghua.edu.cn
+# pip3 install torchinfo
+# pip3 install tqdm
